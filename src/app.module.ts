3687470c3a3136cb06bbe89522b203d4ce64773b
korrawee <korrawee.somyos@gmail.com>
2023-01-11 15:56:26 +0700

    Add : typeorm config and env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ormConfig } from './database/config/ormconfig';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    TypeOrmModule.forRoot(ormConfig()),
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/database/config/ormconfig.ts
@@ -0,0 +1,23 @@
+export function ormConfig(): any {
+  return {
+    type: process.env.DB_TYPE,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    synchronize: true,
+    logging: false,
+    autoLoadEntities: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    entities: ['dist/**/entity/*.entity.js'],
+    migrations: ['dist/database/migrations/*.js'],
+    subscribers: ['dist/observers/subscribers/*.subscriber.js'],
+    cli: {
+      entitiesDir: 'src/components/**/entity',
+      migrationsDir: 'src/database/migrations',
+      subscribersDir: 'src/observers/subscribers',
+    },
+  };
+}
