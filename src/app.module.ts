12c14d1e5e28ce5c12930ee20e9d1ff82a67cdf5
korrawee <korrawee.somyos@gmail.com>
2023-01-11 16:11:28 +0700

    Add : init url component

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ormConfig } from './database/config/ormconfig';
+import { UrlModule } from './components/url/url.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     TypeOrmModule.forRoot(ormConfig()),
+    UrlModule,
   ],
   controllers: [AppController],
   providers: [AppService],
