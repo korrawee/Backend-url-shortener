63506d4615f90f0c8ee231e963545d79791e05ae
korrawee <korrawee.somyos@gmail.com>
2023-01-11 16:20:12 +0700

    Add : method redirect and createUrl

--- src/components/url/url.controller.ts
+++ src/components/url/url.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
+import { CreateUrlDto } from './dtos/create.dto';
+import { UrlService } from './url.service';
 
 @Controller('url')
-export class UrlController {}
+export class UrlController {
+  constructor(private readonly urlService: UrlService) {}
+
+  @Get(':urlId')
+  async redirect(@Res() res, @Param('urlId') urlId: string) {
+    const url = await this.urlService.redirect(urlId);
+    return res.redirect(url.originalUrl);
+  }
+
+  @Post('shorten-url')
+  createUrl(@Body() body: CreateUrlDto) {
+    return this.urlService.createUrl(body.originalUrl);
+  }
+}
