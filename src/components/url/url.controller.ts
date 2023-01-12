af06f5444d097bcb28b7c38e1d91f5093b1f5376
korrawee <korrawee.somyos@gmail.com>
2023-01-12 15:14:37 +0700

    Modify : uri path

--- src/components/url/url.controller.ts
+++ src/components/url/url.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
 import { CreateUrlDto } from './dtos/create.dto';
 import { UrlService } from './url.service';
 
-@Controller('shorturl')
+@Controller('')
 export class UrlController {
   constructor(private readonly urlService: UrlService) {}
 
@@ -12,7 +12,7 @@ export class UrlController {
     return res.redirect(url.originalUrl);
   }
 
-  @Post('')
+  @Post('shorturl')
   async createUrl(@Body() body: CreateUrlDto) {
     return await this.urlService.createUrl(body.originalUrl);
   }
